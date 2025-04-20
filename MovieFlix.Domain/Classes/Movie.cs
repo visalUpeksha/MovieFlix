632b14032324ca921012d7c8a2ec98ac52fa9bbd
punkBlaster <punkblaster1995@gmail.com>
2025-04-20 21:19:03 +0530

    Add project files.

---
+++ MovieFlix.Domain/Classes/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFlix.Domain.Classes
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string MovieName { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
+    }
+}
